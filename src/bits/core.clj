cb6e264154f5a13a3344a6fa03242e180fe9402c
Nikita Prokopov <prokopov@gmail.com>
2018-03-03 17:06:15 +0300

    tools.reader body reader and parser

--- src/bits/core.clj
+++ src/bits/core.clj
@@ -2,48 +2,175 @@
   (:refer-clojure :exclude [require])
   (:require
     [clojure.edn :as edn]
-    [clojure.string :as str]))
+    [clojure.string :as str]
+    [clojure.tools.reader :as reader]
+    [clojure.tools.reader.reader-types :as reader-types]))
 
 
-(defn- parse-fn [form]
-  (assert (= (first form) 'fn))
-  (let [form    (next form)
-        name    (when (symbol? (first form)) (first form))
-        form    (if (some? name) (next form) form)
-        arities (if (vector? (first form)) [form] form)]
-    { :arities  arities
-      :arglists (map first arities)
-      :name     name }))
+(defmacro cond+ [& clauses]
+  (when clauses
+    (let [[c1 c2 & cs] clauses]
+      (cond
+        (< (count clauses) 2) (throw (IllegalArgumentException. "cond requires an even number of forms"))
+        (= c1 :let)          `(let ~c2 (cond+ ~@cs))
+        (= c1 :do)           `(do ~c2 (cond+ ~@cs))
+        (= c1 :when-some)    `(if-some ~c2 ~(first cs) (cond+ ~@(next cs)))
+        :else                `(if ~c1 ~c2 (cond+ ~@cs))))))
 
 
-(defn- require1 [nssym ns sym]
+(defn read-clojure
+  ([s]
+    (binding [reader/*read-eval* false
+              reader/*alias-map* {}]
+      (reader/read
+        { :read-cond :preserve }
+        (reader-types/indexing-push-back-reader s))))
+  ([s feature]
+    (binding [reader/*read-eval* false
+              reader/*alias-map* {}]
+      (reader/read
+        { :read-cond :allow, :features #{feature} }
+        (reader-types/indexing-push-back-reader s)))))
+
+
+(defn underline
+  ([s]
+    (apply str s "\n" (repeat (count s) "^")))
+  ([s pos]
+    (if (neg? pos)
+      (recur s (+ (count s) pos))
+      (str s "\n" (str/join (repeat pos " ")) "^"))))
+
+
+; (defn name docstring meta2? [arglist*] body)
+; (defn name docstring meta2? ([arglist*] body)+ meta3?)
+
+(defn parse-defn-form [form]
+  (cond+
+    (not (list? form))
+    {:code (underline (pr-str form)) :message "Expected list"}
+
+    :let [[defn name docstring & fdecl] form]
+
+    (< (count form) 1)
+    {:code (underline (pr-str form) -1) :message "Expected `defn`"}
+
+    (not= defn 'defn)
+    {:code (underline (pr-str defn)) :message "Expected `defn`"}
+
+    (< (count form) 2)
+    {:code (underline (pr-str form) -1) :message "Expected symbol after `defn`"}
+    
+    (not (symbol? name))
+    {:code (underline (pr-str name)) :message "Expected symbol"}
+
+    (not (simple-symbol? name))
+    {:code (underline (pr-str name)) :message "Expected non-namespaced symbol"}
+
+    :let [m1 (meta name)]
+
+    (< (count form) 3)
+    {:code (underline (pr-str form) -1) :message "Expected docstring, mandatory"}
+
+    (not (string? docstring))
+    {:code (underline (str name " ") -1) :message "Expected docstring, mandatory"}
+
+    (str/blank? docstring)
+    {:code (underline (pr-str docstring)) :message "I see what you did here. No, docstring is still mandatory"}
+
+    :let [[m2 fdecl] (if (map? (first fdecl))
+                       [(first fdecl) (next fdecl)]
+                       [nil fdecl])]
+    
+    (empty? fdecl)
+    {:code (underline (pr-str form) -1) :message "Expected arglist vector"}
+
+    (and (not (vector? (first fdecl)))
+         (not (list? (first fdecl))))
+    {:code (underline (pr-str (first fdecl))) :message "Expected arglist vector"}
+
+    :let [[bodies m3] (cond
+                        (vector? (first fdecl)) [[fdecl] nil]
+                        (map? (last fdecl))     [(butlast fdecl) (last fdecl)]
+                        :else                   [fdecl nil])]
+
+    :when-some [not-list (first (remove list? bodies))]
+    {:code (underline (pr-str not-list)) :message "Expected list"}
+
+    :when-some [empty (first (filter empty? bodies))]
+    {:code (underline (pr-str empty)) :message "Expected arglist"}
+
+    :when-some [no-arglist (first (remove #(vector? (first %)) bodies))]
+    {:code (underline (pr-str (first no-arglist))) :message "Expected arglist vector"}
+
+    :else
+    { :name      name
+      :meta      (merge m1 m2 m3)
+      :docstring docstring
+      :bodies    bodies
+      :arglists  (map first bodies) }))
+
+
+(defmacro one-of? [x & opts]
+  (let [xsym (gensym)]
+   `(let [~xsym ~x]
+      (or ~@(map (fn [o] `(= ~xsym ~o)) opts)))))
+
+
+(defn- escape-char [ch]
+  (cond
+    (<= (int \a) (int ch) (int \z))   ch
+    (<= (int \A) (int ch) (int \Z))   ch
+    (<= (int \0) (int ch) (int \9))   ch
+    (one-of? ch \_ \- \. \+ \! \= \/) ch
+    (<= 128 (int ch))                 ch
+    :else (str "(" (str/upper-case (Integer/toString (int ch) 16)) ")")))
+
+
+(defn fqn->path [fqn]
+  (str/join (map escape-char fqn)))
+
+
+(defn path->fqn [path]
+  (str/replace path #"\(([0-9A-F]+)\)"
+    (fn [[_ hex]]
+      (str (char (Integer/parseInt hex 16))))))
+
+
+(defn fetch [fqn]
+  (slurp (str "/Users/prokopov/Dropbox/ws/clojure-bits-server/bits/" (fqn->path fqn) ".cljc"))) ;; FIXME server url
+
+
+(defn- require-bit [nssym ns sym]
   (when-not (.startsWith (name ns) "bits.")
     (throw (Exception. "Only the bits.* namespaces can be required")))
-  (let [file (str "../clojure-bits-server/"
-                  (-> (str ns "/" sym)
-                      (str/replace "." "/")
-                      (str/replace "-" "_")
-                      (str ".edn")))
-        {_meta :meta
-         _body :body} (edn/read-string (slurp file))
-        {_arglists :arglists} (parse-fn _body)]
+  (let [fn (parse-defn-form (read-clojure (fetch (str ns "/" sym)) :clj)) ;; TODO detect env
+        {:keys [meta docstring bodies arglists]} fn]
    `(intern ~nssym
-            (with-meta '~sym (assoc ~_meta :arglists '~_arglists))
-            ; (with-meta ~_body ~(meta _body)))))
-            ~_body)))
+            (with-meta '~sym (assoc ~meta :arglists '~arglists :doc ~docstring))
+            (fn ~sym ~@bodies))))
 
 
-(defmacro require
-  "(require [`sym`+] :from `ns` :as `ns-alias`)"
-  [syms & {:keys [from as]}]
-  (let [nssym (gensym "ns")]
+(defmacro require1 [spec]
+  (let [[from & {as :as [& syms] :just}] spec
+        nssym (gensym "ns")]
     (concat 
       `(let [~nssym (create-ns '~from)])
-      (map #(require1 nssym from %) syms)
+      (map #(require-bit nssym from %) syms)
       (when (some? as)
         [`(alias '~as '~from)]))))
+  
+
+(defmacro require
+  "(require [`ns` :as `ns-alias` :just [`sym`+]]+)"
+  [& specs]
+  (cons `do
+    (for [spec specs]
+      `(require1 ~spec))))
 
 
 (comment
-  (macroexpand '(req [find zip] :from bits.tonsky.coll :as coll))
+  (require [bits.tonsky.coll :just [find zip]])
+  (require [bits.tonsky.string :just [left-pad]])
+  (clojure.pprint/pprint (macroexpand '(require [bits.tonsky.string :just [left-pad]])))
 )
